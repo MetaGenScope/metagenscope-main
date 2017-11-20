9f1363bcaa6622562ce97d814a2518778c17e2f0
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2017-11-20 18:17:59 -0500

    Fixed duplicate account creation in log in/out test.

--- e2e/tests/login.test.js
+++ e2e/tests/login.test.js
@@ -14,7 +14,7 @@ test('should display the sign in form', async (t) => {
     .expect(Selector('form').exists).ok();
 });
 
-test('should allow a user to sign in', async (t) => {
+test('should allow a user to sign in and out', async (t) => {
   // Register user
   await t
     .navigateTo(`${TEST_URL}/register`)
@@ -25,40 +25,7 @@ test('should allow a user to sign in', async (t) => {
 
   // Log a user out
   await t
-    .click(Selector('a').withText('Account'));
-  await t
-    .click(Selector('a').withText('Log Out'));
-
-  // Log a user in
-  await t
-    .navigateTo(`${TEST_URL}/login`)
-    .typeText('input[name="email"]', email)
-    .typeText('input[name="password"]', 'test')
-    .click(Selector('input[type="submit"]'));
-
-  // Assert user is redirected to '/'
-  // Assert '/' is displayed properly
-  await t
-    .expect(Selector('H1').withText('Metagenomes, Visualized').exists).ok()
-    .expect(Selector('a').withText('User Status').exists).ok()
-    .expect(Selector('a').withText('Log Out').exists).ok()
-    .expect(Selector('a').withText('Register').exists).notOk()
-    .expect(Selector('a').withText('Log In').exists).notOk();
-});
-
-test('should allow a user to sign in', async (t) => {
-  // Register user
-  await t
-    .navigateTo(`${TEST_URL}/register`)
-    .typeText('input[name="username"]', username)
-    .typeText('input[name="email"]', email)
-    .typeText('input[name="password"]', 'test')
-    .click(Selector('input[type="submit"]'));
-
-  // Log a user out
-  await t
-    .click(Selector('a').withText('Account'));
-  await t
+    .click(Selector('a').withText('Account'))
     .click(Selector('a').withText('Log Out'));
 
   // Log a user in
@@ -79,8 +46,7 @@ test('should allow a user to sign in', async (t) => {
 
   // Log a user out
   await t
-    .click(Selector('a').withText('Account'));
-  await t
+    .click(Selector('a').withText('Account'))
     .click(Selector('a').withText('Log Out'));
 
   // Assert '/logout' is displayed properly
