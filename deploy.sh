fc84a8e2cd2fbed74f534ee2ea2e7d0e280328a9
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2017-12-07 11:38:19 -0500

    Pull latest changes BEFORE running deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -x;
-git pull;
 docker-compose -f docker-compose.prod.yml up --no-deps -d metagenscope-service;
 docker-compose -f docker-compose.prod.yml up --no-deps -d web-service;
 docker-compose -f docker-compose.prod.yml up --no-deps -d nginx;
