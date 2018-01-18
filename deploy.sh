0de28ced7582022d6d8a6d894a8aeeb2d5d6ed1b
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-01-18 14:30:28 -0500

    Add 'docker-compose pull' to deploy script. [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x;
+docker-compose -f docker-compose.prod.yml pull;
 docker-compose -f docker-compose.prod.yml up --no-deps -d metagenscope-service;
 docker-compose -f docker-compose.prod.yml up --no-deps -d web-service;
 docker-compose -f docker-compose.prod.yml up --no-deps -d nginx;
