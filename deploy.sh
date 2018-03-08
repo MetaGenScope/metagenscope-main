5237a42af6bf1aa2e4003e2d4a01c23705e45cf9
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-08 14:02:15 -0500

    Add DB migration script to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 set -x;
 docker-compose -f docker-compose.prod.yml pull;
 docker-compose -f docker-compose.prod.yml up --no-deps -d metagenscope-service;
+docker-compose -f docker-compose.prod.yml exec metagenscope-service python manage.py db upgrade;
 docker-compose -f docker-compose.prod.yml up --no-deps -d web-service;
 docker-compose -f docker-compose.prod.yml up --no-deps --force-recreate -d nginx;
