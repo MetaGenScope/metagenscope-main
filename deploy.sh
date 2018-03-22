5c4a328cd06803da2e955645379bfa29c8d44eb3
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-22 09:12:37 -0400

    Tighten up deploy process.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,5 @@
 
 set -x;
 docker-compose -f docker-compose.prod.yml pull;
-docker-compose -f docker-compose.prod.yml up --no-deps -d metagenscope-service;
-docker-compose -f docker-compose.prod.yml exec metagenscope-service python manage.py db upgrade;
-docker-compose -f docker-compose.prod.yml up --no-deps -d web-service;
-docker-compose -f docker-compose.prod.yml up --no-deps --force-recreate -d nginx;
+docker-compose -f docker-compose.prod.yml run metagenscope-service python manage.py db upgrade;
+docker-compose -f docker-compose.prod.yml up --no-deps -d --force-recreate;
