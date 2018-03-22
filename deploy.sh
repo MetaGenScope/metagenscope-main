c50280b57795737f2a8664bd53b87595bf215f7b
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-22 10:09:02 -0400

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -x;
+docker-compose -f docker-compose.prod.yml down;
 docker-compose -f docker-compose.prod.yml pull;
-docker-compose -f docker-compose.prod.yml run metagenscope-service python manage.py db upgrade;
+docker-compose -f docker-compose.prod.yml run --rm metagenscope-service python manage.py db upgrade;
+
+# --rm only removes metagenscope-service but we need all to be down before recreating
+docker-compose -f docker-compose.prod.yml down;
+
 docker-compose -f docker-compose.prod.yml up --no-deps -d --force-recreate;
